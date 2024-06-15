7845db3227a4620119347c26eccf83b84ee2c042
Vitaliy Murzin <murzinvit@yandex.ru>
2024-06-15 20:38:31 +0500

    step 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.util.List;
 import java.util.Random;
 
 public class Main {
-
     public static void main(String[] args) throws InterruptedException {
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
